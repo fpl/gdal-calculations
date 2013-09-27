dee35af5ac77df3415ffbe47a1fe1bede504fdcc
lukepinnerau <lukepinnerau@b8bf4c51-e9db-6fcb-c41f-325b9d4ae042>
2013-09-27 04:53:46 +0000

    Deleted testing123

--- branches/testing123/bin/gdal_calculate.cmd
+++
@@ -1,1 +0,0 @@
-@python -m gdal_calculations %*
--- branches/testing123/setup-bdist.cmd
+++
@@ -1,5 +0,0 @@
-python setup.py sdist --formats=gztar
-python setup.py bdist --format=wininst --plat-name=win-amd64
-python setup.py bdist --format=wininst --plat-name=win32
-rmdir /S /Q build
-pause
