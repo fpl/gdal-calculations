bbeb7f4af98225e29fac67d3c8b81a66965e66ed
lukepinnerau@gmail.com <lukepinnerau@gmail.com@b8bf4c51-e9db-6fcb-c41f-325b9d4ae042>
2014-03-12 02:21:22 +0000

    Untag 0.9

--- tags/0.9/setup-bdist.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python setup.py sdist --formats=gztar
-python setup.py bdist --format=wininst --plat-name=win-amd64
-python setup.py bdist --format=wininst --plat-name=win32
-rm -rf build
-
